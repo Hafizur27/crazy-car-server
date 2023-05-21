364ab6fba9e6738184f8154299e0c3a338a23167
hafizur27 <mahmadhafiz33@gmail.com>
2023-05-21 21:33:35 +0600

    final sumbmission of server site

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.lo1m20r.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+   
     await client.connect();
 
     const toysCollection = client.db("toysDB").collection("toys");
@@ -92,6 +91,23 @@ async function run() {
       res.send(result);
     });
 
+    app.put('/myToys/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updatedToy = req.body;
+      const toy ={
+        $set:{
+          price: updatedToy.price, 
+          quantity: updatedToy.quantity, 
+          description: updatedToy.description
+        }
+      }
+
+      const result = await toysCollection.updateOne(filter, toy, options);
+      res.send(result);
+    });
+
     app.delete('/toys/:id', async(req, res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
@@ -100,12 +116,11 @@ async function run() {
     });
 
 
-    // Send a ping to confirm a successful connection
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
